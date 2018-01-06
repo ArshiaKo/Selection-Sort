a1660f990379ff522cfd6f2905e91fee8949a95b
Arshia <arshiakohanteb@gmail.com>
2018-01-05 17:32:08 -0800

    Finally Started

--- Selection Sort/Selection Sort/Program.cs
+++ Selection Sort/Selection Sort/Program.cs
@@ -10,7 +10,64 @@ namespace Selection_Sort
     {
         static void Main(string[] args)
         {
+            List<int> ints = new List<int>();
+            Random rand = new Random();
+            bool random = true;
+            Console.WriteLine("Do you want to type in 10 numbers or random \ntype 10 or random");
+            string choice = Console.ReadLine();
+            #region ask for 10 numbers
+            if (choice == "10")
+            {
+                random = false;
+            }
 
+            else if (choice == "random" || choice == "")
+            {
+                random = true;
+            }
+
+            if (random)
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    ints.Add(rand.Next(1,101));
+                }
+            }
+
+            if (!random)
+            {
+                Console.WriteLine("Give me 10 numbers");
+                for (int i = 0; i < 10; i++)
+                {
+                    ints.Add(int.Parse(Console.ReadLine()));
+                }
+                
+            }
+            Console.WriteLine("");
+            foreach (int number in ints)
+            {
+                Console.WriteLine(number);
+            }
+            #endregion
+            #region checking for swap
+            int smallest = int.MaxValue;
+            int index = -1;
+            for (int i = 0; i < ints.Count - 1; i++)
+            {
+                for (int j = i; j < ints.Count; j++)
+                {
+                    if (ints[j] < smallest)
+                    {
+                        index = j;
+                        smallest = ints[j];
+                        ints[j] = ints[index];
+                        
+
+                    }
+                }
+            }
+            #endregion
+            Console.ReadKey();
         }
     }
 }
